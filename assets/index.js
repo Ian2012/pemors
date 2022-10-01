4345e0cb136da962b13fd9d05fef805b9517e32c
Cristhian Garcia <crisgarta8@gmail.com>
2022-10-01 14:59:11 -0500

    feat: react integrated

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,7 @@
-function component() {
-  const element = document.createElement('div');
-  element.innerHTML = 'Hello webpack';
-  return element;
-}
-document.body.appendChild(component());
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<h1>Hello, react!</h1>);
