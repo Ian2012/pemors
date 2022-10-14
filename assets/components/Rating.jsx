b66a4581e4d7747bcc3962a5494b73d0338a20b4
Cristhian Garcia <crisgarta8@gmail.com>
2022-10-13 22:06:08 -0500

    style: changes in react frontend

--- assets/components/Rating.jsx
+++ assets/components/Rating.jsx
@@ -8,22 +8,24 @@ export function Rating({movie, callback}) {
             method: "POST",
             body: JSON.stringify({'title': movie.id, 'rating': rating}),
             headers: {
-              "Content-type": "application/json;charset=UTF-8",
-              "X-CSRFToken": csrftoken
+                "Content-type": "application/json;charset=UTF-8",
+                "X-CSRFToken": csrftoken
             },
             mode: 'same-origin'
-          })
+        })
             .then(response => response.json())
             .then(json => console.log(json))
             .catch(err => console.log(err));
         callback()
     };
 
-    return <ReactStars
-        classNames={"flex items-center"}
-        count={10}
-        onChange={ratingChanged}
-        size={52}
-        activeColor="#ffd700"
-    />
+    return <div className="flex justify-center">
+        <ReactStars
+            classNames={"flex"}
+            count={10}
+            onChange={ratingChanged}
+            size={52}
+            activeColor="#ffd700"
+        />
+    </div>
 }
--- assets/components/Title.jsx
+++ assets/components/Title.jsx
@@ -20,27 +20,26 @@ export function Title({movie, callback}) {
     }
     useEffect(() => fetchData())
 
-    return <section className="text-gray-600 body-font overflow-hidden">
+    return <section className="text-gray-100 body-font overflow-hidden">
         <div className="container px-5 py-24 mx-auto">
-            <div className="lg:w-4/5 mx-auto flex flex-wrap">
+            <div className="lg:w-4/5 mx-auto flex flex-wrap justify-center">
                 {poster && <img alt="ecommerce"
-                                className="lg:w-1/2 max-h-[550px] lg:h-auto h-64 object-contain object-center rounded"
+                                className="min-h-[32rem] lg:w-1/2 max-h-[550px] lg:h-auto h-64 object-contain object-center rounded"
                                 src={poster}></img>}
                 <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-                    <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">{movie.primary_title}</h1>
+                    <h1 className="text-3xl title-font font-medium mb-1">{movie.primary_title}</h1>
                     <div className="flex mb-4">
-                        <span className="flex items-center">
+                        <span className="flex">
                             {movie.omdb && <span
-                                className="text-gray-600 ml-3">{movie.omdb.imdbVotes === "N/A" ? 0 : movie.omdb.imdbVotes} Reviews</span>}
-                        </span>
-                        <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
-                            Useful links
+                                className="text-gray-400">{movie.omdb.imdbVotes === "N/A" ? 0 : movie.omdb.imdbVotes} Reviews</span>}
                         </span>
                     </div>
-                    {movie.omdb && <p className="leading-relaxed">
+                    {movie.omdb && <p className="leading-relaxed md:text-blue">
                         {movie.omdb.Plot}
                     </p>}
-                    <Rating key={new Date().getTime()} movie={movie} callback={callback}></Rating>
+                    <div className="mx-auto">
+                        <Rating key={new Date().getTime()} movie={movie} callback={callback}></Rating>
+                    </div>
                 </div>
             </div>
         </div>
