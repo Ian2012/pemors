e9324312d9a2ce50842a6acea6e8d3fba12f27fc
Cristhian Garcia <crisgarta8@gmail.com>
2022-10-23 14:25:08 -0500

    feat: show user personality in user detail

--- pemors/users/views.py
+++ pemors/users/views.py
@@ -1,45 +1,30 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import redirect, render
 from django.urls import reverse
-from django.utils.translation import gettext_lazy as _
-from django.views.generic import DetailView, RedirectView, UpdateView
+from django.views.generic import RedirectView, TemplateView
 
+from pemors.titles.models import UserRating
 from pemors.users.forms import QUESTIONS, UserPersonalityForm
 from pemors.users.models import Profile
 
 User = get_user_model()
 
 
-class UserDetailView(LoginRequiredMixin, DetailView):
+class UserDetailView(LoginRequiredMixin, TemplateView):
     model = User
-    slug_field = "username"
-    slug_url_kwarg = "username"
+    template_name = "users/user_detail.html"
 
     def get_context_data(self, **kwargs):
-        return super().get_context_data(**kwargs)
-
-    def get_object(self, **kwargs):
-        return self.request.user
+        context = super().get_context_data(**kwargs)
+        return context
 
 
 user_detail_view = UserDetailView.as_view()
 
 
-class UserUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
-    model = User
-    fields = ["name"]
-    success_message = _("Information successfully updated")
-
-    def get_success_url(self):
-        assert (
-            self.request.user.is_authenticated
-        )  # for mypy to know that the user is authenticated
-        return self.request.user.get_absolute_url()
-
-    def get_object(self, **kwargs):
-        return self.request.user
+class UserUpdateView(LoginRequiredMixin, TemplateView):
+    model = UserRating
 
 
 user_update_view = UserUpdateView.as_view()
